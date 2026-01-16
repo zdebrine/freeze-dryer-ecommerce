33c37f54d9e8b1e7e104155c2ebcd5684df3ba44
Zak DeBrine <zdebrine@gmail.com>
2026-01-16 11:05:45 -0600

    Added storybook

--- stories/Input.stories.tsx
+++ stories/Input.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react"
+import type { Meta, StoryObj } from "@storybook/nextjs"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
